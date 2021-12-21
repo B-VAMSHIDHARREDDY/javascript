dfc192f7980b8e646a8973d11abf0eef5b4ecf1c
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2021-12-21 12:24:48 +0530

    21-12-2021

---
+++ youtube_videos_code/fun.js
@@ -0,0 +1,13 @@
+// Performing a task    
+
+function greet(name,lastname){
+    console.log("Hello" + name + ' '+lastname);
+}
+greet("Beecharla","Vamshi");
+greet("Reddy","Beecharla");
+
+function square(number){
+    return number * number;
+}
+let number = square(2)
+console.log(number)
---
+++ youtube_videos_code/index.js
@@ -0,0 +1,36 @@
+console.log("Welcome world")
+let name = 'Vamshi';
+console.log(name);
+
+let firstName='Vamshi';
+let lastName='Beecharla';
+//let FirstName;
+
+let interestRate = 0.3;
+// interestRate = 1;
+console.log(interestRate);
+
+let names = undefined;
+let selectedColor = null;
+
+let age = 23;
+let person = {
+    name : 'vamshi',
+    age :23
+}
+console.log(person.name);
+// Dot Notation
+person.name = 'Reddy';
+console.log(person.name);
+
+// Bracket Notation
+person['name']='New person';
+console.log(person.name);
+
+// Bracket notation dynamic notation
+
+let selection = 'name'
+person[selection] = 'Beecharla'
+console.log(person.name);
+
+
