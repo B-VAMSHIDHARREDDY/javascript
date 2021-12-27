f56ed6edb6b102fc4ae7d9998b81fcc81d1e9c65
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2021-12-27 18:51:26 +0530

    27-12-2021

---
+++ ReactDemos/myreactapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AppHeader from './AppHeader';
+function App() {
+  return (
+    <div>
+      {/* <h1>
+        Welcome to react Vamshi
+     </h1> */}
+     <AppHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactDemos/myreactapp/src/AppHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AppHeader extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App Header</h1>
+                <hr/>
+            </div>
+        )
+    }
+}
